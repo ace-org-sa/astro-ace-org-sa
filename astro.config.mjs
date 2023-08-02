02f51beb4eedde7d6923dbb22999387f9911c178
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2023-08-02 22:08:54 +0300

    ..

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ace.org.sa',
-  integrations: [tailwind()]
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp',
+  })]
 });
